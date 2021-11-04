ca5537c7b55d6962504fbdb29436e3f0efc082eb
davaryoga <92939686+davaryoga@users.noreply.github.com>
2021-11-05 03:14:44 +0700

    Add files via upload

---
+++ luas_dan_keliling_lingkaran.py
@@ -0,0 +1,12 @@
+print("========================================")
+print("Menghitung Luas dan Keliling Lingkaran")
+print("   Muhammad Dava Aryoga (312110552)")
+print("========================================")
+import math
+r = float(input("Masukan Jari-jari : "))
+
+luas = math.pi*(r*r)
+keliling = 2*math.pi*r
+
+print ("Luas Lingkaran \t\t= ",luas)
+print ("Keliling Lingkaran\t= ",keliling)
